7ae729f87ef3b54bc163b5e344b11b0640811417
kyra9502 <kyradoc9502@hotmail.com>
2018-08-16 15:39:39 +0200

    test 2

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -6,10 +6,10 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-if (empty($_POST['name']) ||
-empty($_POST['email']) ||
-empty($_POST['message']) ||
-!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if (empty(htmlspecialchars($_POST['name'])) ||
+empty(htmlspecialchars($_POST['email'])) ||
+empty(htmlspecialchars($_POST['message'])) ||
+!filter_var(htmlspecialchars($_POST['email']), FILTER_VALIDATE_EMAIL)) {
     echo "No arguments Provided!";
     return false;
 }
